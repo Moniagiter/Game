a9691085dfdffe119a880c3dfe88344879730054
Monia <mykhailo.bilobrovko@stud.onu.edu.ua>
2024-11-13 21:23:38 +0200

    Add

---
+++ src/Grid.java
@@ -0,0 +1,37 @@
+
+public class Grid {
+    private final int size;
+    private final char[][] grid;
+
+    public Grid(int size) {
+        this.size = size;
+        this.grid = new char[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                grid[i][j] = '.';
+            }
+        }
+    }
+
+    public void updateGrid(int x, int y, char symbol) {
+        grid[x][y] = symbol;
+    }
+
+    public void display() {
+        System.out.print("  ");
+        for (int i = 1; i <= size; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < size; i++) {
+            System.out.print((char) ('A' + i) + " ");
+            for (int j = 0; j < size; j++) {
+                System.out.print(grid[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+
