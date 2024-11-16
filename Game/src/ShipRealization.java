e574bb53814cf0937a2a1e9e0b510683e1872507
Monia <mykhailo.bilobrovko@stud.onu.edu.ua>
2024-11-16 11:08:12 +0200

    Create ShipRealization.java

---
+++ Game/src/ShipRealization.java
@@ -0,0 +1,34 @@
+import java.util.List;
+
+public class ShipRealization implements Ship {
+    private final int size;
+    private final List<int[]> coordinates;
+    private int hits;
+
+    public ShipRealization(int size, List<int[]> coordinates) {
+        this.size = size;
+        this.coordinates = coordinates;
+        this.hits = 0;
+    }
+
+    @Override
+    public boolean isHit(int x, int y) {
+        for (int[] coord : coordinates) {
+            if (coord[0] == x && coord[1] == y) {
+                hits++;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean isSunk() {
+        return hits == size;
+    }
+
+    @Override
+    public List<int[]> getCoordinates() {
+        return coordinates;
+    }
+}
