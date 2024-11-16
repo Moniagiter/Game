d7d9b3daedb837b3ae6e39b0cf5564f2eb8eedd5
Monia <mykhailo.bilobrovko@stud.onu.edu.ua>
2024-11-16 11:07:39 +0200

    Update Ship.java

--- Game/src/Ship.java
+++ Game/src/Ship.java
@@ -1,32 +1,8 @@
 
 import java.util.List;
 
-public class Ship {
-    private final int size;
-    private final List<int[]> coordinates;
-    private int hits;
-
-    public Ship(int size, List<int[]> coordinates) {
-        this.size = size;
-        this.coordinates = coordinates;
-        this.hits = 0;
-    }
-
-    public boolean isHit(int x, int y) {
-        for (int[] coord : coordinates) {
-            if (coord[0] == x && coord[1] == y) {
-                hits++;
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public boolean isSunk() {
-        return hits == size;
-    }
-
-    public List<int[]> getCoordinates() {
-        return coordinates;
-    }
+public interface Ship {
+    boolean isHit(int x, int y);
+    boolean isSunk();
+    List<int[]> getCoordinates();
 }
